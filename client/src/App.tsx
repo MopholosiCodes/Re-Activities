453517d2aa7cc65d692304cb9281c1d2c1f8634e
Mopholosi <67508554+Mopholosi@users.noreply.github.com>
2023-01-24 20:44:49 +0200

    Client Skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,25 @@
 import React,{useState, useEffect} from 'react';
 import axios from "axios";
-import logo from './logo.svg';
 import './App.css';
+import { Header } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get("https://localhost:5000/api/Activities")
-      .then(response => {
-        console.table(response.data)
-        setActivities(response.data)
-      })
+    axios
+      .get("https://localhost:5000/api/Activities")
+      .then(response => setActivities(response.data))
   },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+    <div>
+      <Header as="h1" icon='users' content="Re-Activities" />
+      <ul>
           {activities.map((activity: any) => (
             <li key={activity.id}>{activity.title}</li>
           ))}
         </ul>
-      </header>
     </div>
   );
 }
